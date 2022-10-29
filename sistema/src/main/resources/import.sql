0b968401a83ada0f1cb9ca2dfe8a8c20313c6dcc
Felipe <soares.felipe200@gmail.com>
2022-10-29 16:29:08 -0300

    incluindo import sql

--- sistema/src/main/resources/import.sql
+++ sistema/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 insert into cliente (cpf,nome,telefone) values ('02187354644','Felipe Soares','38998605529');
 insert into cliente (cpf,nome,telefone) values ('01304446697','Elisabete Xavier','38999110296');
 insert into cliente (cpf,nome,telefone) values ('07697785692','Josimar Viana','38999302681');
+insert into veiculo (veiculo,placa,ano_fabricacao,ano_modelo,marca,modelo,preco,quantidade_portas) values ('carro','PUW-1803','2014','2015','CHEVROLET','CELTA','33982','4');
+insert into veiculo (veiculo,placa,ano_fabricacao,ano_modelo,marca,modelo,preco,quantidade_passageiros,quantidade_eixos) values ('onibus','EJB-3425','2004','2005','SCANIA','K94','45000','15','5');
+insert into veiculo (veiculo,placa,ano_fabricacao,ano_modelo,marca,modelo,preco,capacidade_carga) values ('caminhao','PLV-3894','2007','2008','VOLVO','FH-540','500000','74000');
